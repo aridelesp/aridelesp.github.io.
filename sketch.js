0a9965eb9fe4ca3b25a03a659c6c92c85ea6d9c0
aridelesp <67489121+aridelesp@users.noreply.github.com>
2020-10-15 10:57:29 -0500

    Probando otro sketch.js

--- sketch.js
+++ sketch.js
@@ -1,136 +1,35 @@
-//var flower1;
-//var flower2;
-//var flower3;
-var flowers = [];
-
 function setup() {
   createCanvas(400, 400);
-  frameRate(5);
-
-  for (i = 0; i < 100; i++) {
-    var x = random(0, width);
-    var y = random(0, height);
-    var t = random(10, 20);
-    flowers[i] = new Flower(x, y, t);
-  }
-
-
-
-
 }
 
 function draw() {
-  background(208, 255, 194);
-
-  for (i = 0; i < 100; i++) {
-    flowers[i].mostrar();
-    flowers[i].mover();
-
-  }
+  background(208,255,194);
+  //Pétalos
+  noStroke(128,0,128);
+fill(210, 115, 255);
+  ellipse(200,110,80,80);
+  //abajo
+   ellipse(140,140,80,80);
+  ellipse(140,260,80,80);
+  ellipse(260,260,80,80);
+  ellipse(260,140,80,80);
+  ellipse(110,200,80,80);
+  ellipse(200,290,80,80);
+  ellipse(290, 200,80,80);
   
  
-
-
-
-
-
-}
-
-class Flower {
-  constructor(tempX, tempY, tempT) {
-    this.x = tempX; //200
-    this.y = tempY; //200
-    this.t = tempT; //80
-
-  }
-  mostrar() {
-    //Pétalos
-    ellipseMode(RADIUS);
-
-    noStroke(128, 0, 128);
-    fill(random(0, 255), random(0, 255), random(0, 255));
-    ellipse(this.x, this.y - this.t * 2.2, this.t, this.t);
-    //abajo
-
-    noStroke(128, 0, 128);
-    fill(random(0, 255), random(0, 255), random(0, 255));
-    ellipse(this.x - this.t * 1.5, this.y - this.t * 1.5, this.t, this.t);
-
-    noStroke(128, 0, 128);
-    fill(random(0, 255), random(0, 255), random(0, 255));
-    ellipse(this.x - this.t * 1.5, this.y + this.t * 1.5, this.t, this.t);
-
-    noStroke(128, 0, 128);
-    fill(random(0, 255), random(0, 255), random(0, 255));
-    ellipse(this.x + this.t * 1.5, this.y + this.t * 1.5, this.t, this.t);
-
-    noStroke(128, 0, 128);
-    fill(random(0, 255), random(0, 255), random(0, 255));
-    ellipse(this.x + this.t * 1.5, this.y - this.t * 1.5, this.t, this.t);
-
-    noStroke(128, 0, 128);
-    fill(random(0, 255), random(0, 255), random(0, 255));
-    ellipse(this.x - this.t * 2.2, this.y, this.t, this.t);
-
-    noStroke(128, 0, 128);
-    fill(random(200, 255), random(200, 255), random(200, 255));
-    ellipse(this.x, this.y + this.t * 2.2, this.t, this.t);
-
-    noStroke(128, 0, 128);
-    fill(random(0, 255), random(0, 255), random(0, 255));
-    ellipse(this.x + this.t * 2.2, this.y, this.t, this.t);
-
-
-
-
-
-
-    //Carita
-    ellipseMode(RADIUS);
-    stroke(0);
-    fill(255, 253, 115);
-    ellipse(this.x, this.y, this.t * 1.25, this.t * 1.25);
-
-    //Ojos
-    fill(0);
-    noStroke();
-    ellipseMode(RADIUS);
-    ellipse(this.x - this.t * 0.5, this.y - this.t * 0.4, this.t * 0.125, this.t * 0.25);
-    ellipse(this.x + this.t * 0.5, this.y - this.t * 0.4, this.t * 0.125, this.t * 0.25);
-
-    //Boca
-    noStroke
-    arc(this.x, this.y + this.t * 0.125, this.t * 0.6, this.t * 0.6, 0, HALF_PI + HALF_PI);
-
-    if (mouseIsPressed) {
-      fill(255, 0, 0);
-    } else {
-      fill(0);
-    }
-    if (mouseIsPressed) {
-      arc(this.x, this.y + this.t * 0.120, this.t * 0.55, this.t * 0.55, 0, HALF_PI + HALF_PI);
-      fill(0);
-      textSize(25);
-      textStyle(ITALIC);
-      text ('Hi!', this.x + 50, this.y);
-      
-
-    }
-
-
-  }
   
-  mover() {
-    this.x = this.x + random (-10, 10);
-    this.y = this.y + random (-10, 10);
-    
-    //this.x = this.x + 1;
-    //this.y = this.y + 1;
-  }
-
+  //Carita
+  stroke(0);
+  fill(255,253,115);
+  ellipse(200, 200, 100, 100);
+  //Ojos
+  fill (0,0,0);
+  noStroke();
+  ellipse(180, 190, 10,20);
+  ellipse(220,190,10,20);
+  //Boca
+  arc(200, 210, 50, 50, 0, HALF_PI+HALF_PI);
+  noStroke
   
-
-
-
-
 }
